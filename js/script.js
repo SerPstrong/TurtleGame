69d25324cc6d40e8b272c33968616d49701c80a1
SerPstrong <ommuts@gmail.com>
2018-08-26 23:38:30 +0500

    css del

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var nine = 9;
+var five = 5;
+var thirtyTwo = 32;
+var c = +prompt("Введите значение по цельсию");
+var f = (nine / five) * c + thirtyTwo;
+alert("Температура по Фаренгейту равна " + f);
+
+var name = "Василий";
+var admin = name;
+alert(admin);
